5a62f838eae1ebb7e0919b343c1e269f9aed55e6
Nayan Washnik <128236231+NayanpR0@users.noreply.github.com>
2023-12-15 23:10:45 +0530

    Update stats.py

--- StringGen/modules/stats.py
+++ StringGen/modules/stats.py
@@ -110,4 +110,4 @@ async def verupikkals(_, message: Message):
         if not done % 20:
             await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nFailed: {failed}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nFailed: {failed")    
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nFailed: {failed}")    
